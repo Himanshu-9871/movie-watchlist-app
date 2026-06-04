6dd2f132211671b2499afb101157076edfc019f8
Himanshu-9871 <hnaroliya2006@gmail.com>
2026-06-04 17:12:43 +0530

    Railway DB Connections

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,8 @@ const connection = mysql_connection.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
 });
 
 //process is the global object in node js gives control over current running processes
